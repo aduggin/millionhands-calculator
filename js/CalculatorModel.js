b1f4ea2f75a0bb604651afc57c5c0429d7aec5c9
Alistair Duggin <alistair.duggin@btinternet.com>
2015-04-14 01:27:59 +0100

    Build: 2 Branch: master  0.0.1 SHA: c6e741b

--- js/CalculatorModel.js
+++ js/CalculatorModel.js
@@ -8,20 +8,20 @@
 
   function CalculatorModel(data) {
     this.email = data.email || 0;
-    this.facebook = data.facebook || 0;
+    this.facebookFan = data.facebookFan || 0;
+    this.facebookDj = data.facebookDj || 0;
     this.instagram = data.instagram || 0;
     this.twitter =  data.twitter || 0;
   }
 
-
   CalculatorModel.prototype.getCalculation = function () {
+    var emailResult = this.email * EMAIL_MULTIPLIER;
+    var facebookFanResult = this.facebookFan * FACEBOOK_MULTIPLIER;
+    var facebookDjResult = this.facebookDj * FACEBOOK_MULTIPLIER;
+    var instagramResult = this.instagram * INSTAGRAM_MULTIPLIER;
+    var twitterResult = this.twitter * TWITTER_MULTIPLIER;
 
-    var emailResult = (this.email * EMAIL_MULTIPLIER) * 12;
-    var facebookResult = (this.facebook * FACEBOOK_MULTIPLIER) * 12;
-    var instagramResult = (this.instagram * INSTAGRAM_MULTIPLIER) * 12;
-    var twitterResult = (this.twitter * TWITTER_MULTIPLIER) * 12;
-
-    return emailResult + facebookResult + instagramResult + twitterResult;
+    return emailResult + facebookFanResult + facebookDjResult + instagramResult + twitterResult ;
   };
 
   global.CalculatorModel = CalculatorModel;
